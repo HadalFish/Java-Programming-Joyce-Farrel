765312179eaceec43c7a982c8c9db55c1e2f2740
HadalFish <kyledawson817@gmail.com>
2025-01-31 22:44:17 -0800

    Add files via upload
    names were changed for the "aliens", if you know then you have good taste.

---
+++ Chapter11/GameZone/1/CreateAliens.java
@@ -0,0 +1,15 @@
+
+public class CreateAliens {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CelestialEmissary celestialemissary = new CelestialEmissary("light glowing blue", "round with tendrils trailing outwards" , "conected");
+		LivingFailures livingfailures = new LivingFailures("a dull gray blue" , "like loose skin draped over a ball", "not connected" );
+		
+				
+				System.out.println(celestialemissary.toString()); 
+				System.out.println(livingfailures.toString());
+	}
+
+}
